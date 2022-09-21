1d5c2c29c3ec7772ec530856097df256ef03ddd7
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2022-09-21 22:50:58 +0300

    Add listings count

--- web/app/components/index/filters/index.tsx
+++ web/app/components/index/filters/index.tsx
@@ -1,15 +1,24 @@
+import { useHits } from "react-instantsearch-hooks-web";
 import CheckboxFilter from "./CheckboxFilter";
 
-const Filters = () => (
-  <div className="w-[25.5rem] h-[610px] sticky top-20 overflow-y-scroll scrollbar-white">
-    <CheckboxFilter enableSearch={true} attribute="designer" />
-    <CheckboxFilter enableSearch={false} attribute="category" />
-    <CheckboxFilter enableSearch={false} attribute="size" />
-    <CheckboxFilter enableSearch={false} attribute="condition" />
-    <CheckboxFilter enableSearch={false} attribute="tags" />
-    <CheckboxFilter enableSearch={false} attribute="price" />
-  </div>
-);
+const Filters = () => {
+  const { results } = useHits();
+  return (
+    <div className="sticky top-20 h-full">
+      <div className="ml-4 mb-3 font-bold underline underline-offset-4">
+        {results?.nbHits} listings
+      </div>
+      <div className="w-[25.5rem] h-[610px] overflow-y-scroll scrollbar-white">
+        <CheckboxFilter enableSearch={true} attribute="designer" />
+        <CheckboxFilter enableSearch={false} attribute="category" />
+        <CheckboxFilter enableSearch={false} attribute="size" />
+        <CheckboxFilter enableSearch={false} attribute="condition" />
+        <CheckboxFilter enableSearch={false} attribute="tags" />
+        <CheckboxFilter enableSearch={false} attribute="price" />
+      </div>
+    </div>
+  );
+};
 
 export default Filters;
 
