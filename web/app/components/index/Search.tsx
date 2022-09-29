edfe73001d14b89cacb28898d7fcce1f22a1048e
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2022-09-30 00:13:51 +0300

    Add clear search

--- web/app/components/index/Search.tsx
+++ web/app/components/index/Search.tsx
@@ -1,11 +1,24 @@
-import { Search } from "@mui/icons-material";
-import { InputAdornment, TextField } from "@mui/material";
+import { Clear, Search } from "@mui/icons-material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { useLocation } from "@remix-run/react";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
 import { useSearchBox } from "react-instantsearch-hooks-web";
 
 const MainSearch = () => {
   const { pathname } = useLocation();
   const { refine } = useSearchBox();
+  const [seachValue, setSearchValue] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    refine(e.currentTarget.value);
+    setSearchValue(e.currentTarget.value);
+  };
+
+  const clearSearch = () => {
+    refine("");
+    setSearchValue("");
+  };
 
   if (pathname !== "/") return null;
 
@@ -14,11 +27,20 @@ const MainSearch = () => {
       className="ml-4 w-[100%] sm:w-[48.5%]"
       placeholder="Search"
       inputProps={{ "aria-label": "search" }}
-      onChange={(e) => refine(e.currentTarget.value)}
+      onChange={onChange}
+      value={seachValue}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
-            <Search />
+            {seachValue ? (
+              <IconButton onClick={clearSearch}>
+                <Clear />
+              </IconButton>
+            ) : (
+              <IconButton>
+                <Search />
+              </IconButton>
+            )}
           </InputAdornment>
         ),
       }}
