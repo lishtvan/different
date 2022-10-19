43756d22ebb0828d5cce296fcbc324fbfd20a51f
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2022-10-19 16:26:14 +0300

    Render swagger only for dev and local envs

--- server/src/plugins/swagger.ts
+++ server/src/plugins/swagger.ts
@@ -3,6 +3,7 @@ import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export default fp(async (fastify) => {
+  if (process.env.NODE_ENV === 'production') return;
   fastify.register(fastifySwagger, {
     mode: 'dynamic',
     openapi: {
