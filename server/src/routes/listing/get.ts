8e4544fbcec4c9a7b2c765d9c6cef7538cb38a26
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2022-11-12 15:05:29 +0200

    Add seller profile to listing page

--- server/src/routes/listing/get.ts
+++ server/src/routes/listing/get.ts
@@ -12,16 +12,30 @@ const schema = {
   } as const,
   response: {
     '2xx': {
-      title: { type: 'string' },
-      designer: { type: 'string' },
-      description: { type: 'string' },
-      tags: { type: 'string' },
-      condition: { type: 'string' },
-      category: { type: 'string' },
-      size: { type: 'string' },
-      shipping: { type: 'array', items: { type: 'string' } },
-      imageUrls: { type: 'array', items: { type: 'string' } },
-      price: { type: 'number' },
+      listing: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          designer: { type: 'string' },
+          description: { type: 'string' },
+          tags: { type: 'string' },
+          condition: { type: 'string' },
+          category: { type: 'string' },
+          size: { type: 'string' },
+          shipping: { type: 'array', items: { type: 'string' } },
+          imageUrls: { type: 'array', items: { type: 'string' } },
+          price: { type: 'number' },
+        },
+      },
+      seller: {
+        type: 'object',
+        properties: {
+          nickname: { type: 'string' },
+          avatarUrl: { type: 'string', nullable: true },
+          name: { type: 'string', nullable: true },
+          id: { type: 'number' },
+        },
+      },
     },
   },
 };
@@ -32,13 +46,19 @@ const getListing: FastifyPluginAsync = async (fastify) => {
   fastify.post<Schema>('/get', { schema }, async (req, reply) => {
     const { listingId } = req.body;
 
-    const listing = await fastify.prisma.listing.findFirst({
+    const listing = await fastify.prisma.listing.findUnique({
       where: {
         id: listingId,
       },
     });
 
-    return reply.send(listing);
+    const seller = await fastify.prisma.user.findUnique({
+      where: {
+        id: listing?.userId,
+      },
+    });
+
+    return reply.send({ listing, seller });
   });
 };
 
--- web/app/routes/listing/$listingId.tsx
+++ web/app/routes/listing/$listingId.tsx
@@ -1,28 +1,27 @@
 import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { fetchInstance } from "~/utils/fetchInstance";
 import ImageGallery from "react-image-gallery";
 import { useMemo } from "react";
 import ukrPoshtaIcon from "./../../assets/ukr-poshta.png";
 import novaposhtaIcon from "./../../assets/nova-poshta.png";
-import { Button } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
+import ProfileImage from "./../../assets/profile.jpeg";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const listingId = Number(params.listingId);
 
-  const listing = await fetchInstance({
+  const listingData = await fetchInstance({
     request,
     route: "/listing/get",
     method: "POST",
     body: { listingId },
   });
-
-  return listing;
+  return listingData;
 };
 
 const ListingRoute = () => {
-  const listing = useLoaderData();
-
+  const { listing, seller } = useLoaderData();
   const { tags, images } = useMemo(() => {
     const formattedImages = listing.imageUrls.map((imageUrl: string) => ({
       original: imageUrl,
@@ -33,22 +32,36 @@ const ListingRoute = () => {
   }, [listing]);
 
   return (
-    <div className="w-full my-3 flex flex-col gap-6 mx-auto justify-center md:flex-row">
-      <div className="min-w-[55%] w-full md:w-[55%]">
+    <div className="w-full my-3 flex flex-col gap-14 mx-auto justify-center md:flex-row">
+      <div className="w-full  md:w-2/5 md:min-w-2/5">
         <ImageGallery infinite showPlayButton={false} items={images} />
       </div>
-      <div className="gap-y-3 flex flex-col mr-9 w-full md:max-w-[30%] md:w-fit">
-        <div className="text-3xl font-bold mb-3">{listing.title}</div>
-        <div className="text-2xl">Designer: {listing.designer}</div>
-        <div className="text-2xl">Size: {listing.size}</div>
-        <div className="text-2xl">Category: {listing.category}</div>
-        <div className="text-2xl">Condition: {listing.condition}</div>
+      <div className="gap-y-3 flex flex-col w-full md:max-w-[30%] md:w-fit">
+        <div className="flex items-center justify-between mb-3">
+          <div>
+            <div className="text-2xl font-bold">{listing.title}</div>
+          </div>
+          <Link
+            className="flex items-center gap-4 ml-3 border-main rounded-md border py-1 px-2"
+            to={`/user/${seller.id}`}
+          >
+            <Avatar
+              sx={{ width: 50, height: 50 }}
+              src={seller?.avatarUrl || ProfileImage}
+            />
+            <div className="text-xl">{seller?.nickname || seller?.name}</div>
+          </Link>
+        </div>
+        <div className="text-xl">Designer: {listing.designer}</div>
+        <div className="text-xl">Size: {listing.size}</div>
+        <div className="text-xl">Category: {listing.category}</div>
+        <div className="text-xl">Condition: {listing.condition}</div>
         {listing?.tags && (
           <div className="flex gap-2 flex-wrap mr-2">
             {tags.map((tag: string) => (
               <div
                 key={tag}
-                className="text-2xl w-fit border-main rounded-md text-main border py-1 px-2"
+                className="text-xl w-fit border-main rounded-md text-main border py-1 px-2"
               >
                 {tag}
               </div>
@@ -63,12 +76,12 @@ const ListingRoute = () => {
             <img src={ukrPoshtaIcon} className="w-36" alt="UkrPoshta" />
           )}
         </div>
-        <div className="text-3xl my-4 font-bold">{listing.price}₴</div>
+        <div className="text-2xl my-4 font-bold">{listing.price}₴</div>
         <Button variant="contained">Purchase</Button>
         {listing.description && (
           <div className="my-6">
-            <div className="text-2xl font-semibold">Description</div>
-            <div className="text-xl mt-2">{listing.description}</div>
+            <div className="text-xl font-semibold">Description</div>
+            <div className="text-lg mt-2">{listing.description}</div>
           </div>
         )}
       </div>
