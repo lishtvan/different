6f75f3219536d68803f6144dda267bfc6a9fbbcf
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2023-01-05 21:43:30 +0200

    Chat v5

--- server/src/routes/chat/message.ts
+++ server/src/routes/chat/message.ts
@@ -2,10 +2,11 @@ import { FastifyPluginAsync } from 'fastify';
 
 const chats = new Map();
 const root: FastifyPluginAsync = async (fastify) => {
-  fastify.get('/message', { websocket: true }, (connection) => {
+  fastify.get('/message', { websocket: true }, (connection, req) => {
     const { socket } = connection;
-    socket.on('message', (message) => {
+    socket.on('message', async (message) => {
       const data = JSON.parse(message.toString());
+      const ownUserId = Number(req.cookies.userId);
 
       let chat = chats.get(data.chatId);
       if (!chat) {
@@ -14,8 +15,44 @@ const root: FastifyPluginAsync = async (fastify) => {
       }
       if (!chat.has(socket)) chat.add(socket);
 
+      if (data.isConnect) {
+        const chat = await fastify.prisma.chat.findUnique({
+          where: { id: Number(data.chatId) },
+          select: {
+            Messages: {
+              orderBy: {
+                createdAt: 'desc',
+              },
+            },
+            Users: {
+              select: {
+                id: true,
+                nickname: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+        });
+        const recipient = chat?.Users.find((user) => user.id !== ownUserId);
+        const sender = chat?.Users.find((user) => user.id === ownUserId);
+        socket.send(JSON.stringify({ chat: { ...chat, Users: { recipient, sender } } }));
+        return;
+      }
+
+      let newMessage;
+      if (data.text) {
+        newMessage = await fastify.prisma.message.create({
+          data: {
+            text: data.text,
+            chatId: Number(data.chatId),
+            senderId: ownUserId,
+          },
+        });
+      }
+
       for (const client of chat) {
-        client.send(JSON.stringify(data));
+        client.send(JSON.stringify(newMessage));
       }
     });
   });
