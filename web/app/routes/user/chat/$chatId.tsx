25bcc8bb3b4c09624ed543b34f20970f6e4a1b19
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2023-01-08 18:40:18 +0200

    Changle local ws domain

--- web/app/routes/user/chat/$chatId.tsx
+++ web/app/routes/user/chat/$chatId.tsx
@@ -21,7 +21,7 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 const WS_DOMAIN_BY_ORIGIN = {
-  "http://localhost:3000": "ws://localhost:3000",
+  "http://localhost:3000": "ws://localhost:8000",
   "https://dev.different-marketplace.com":
     "wss://dev.api.different-marketplace.com",
   "https://different-marketplace.com": "wss://api.different-marketplace.com",
