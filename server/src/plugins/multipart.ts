818d96665f81d338e054021f9e538a5da0f9e5bf
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2023-01-14 20:48:26 +0200

    Remove multipart

--- server/src/plugins/multipart.ts
+++
@@ -1,6 +0,0 @@
-import multipart from '@fastify/multipart';
-import fp from 'fastify-plugin';
-
-export default fp(async (fastify) => {
-  fastify.register(multipart, { attachFieldsToBody: true, sharedSchemaId: 'file' });
-});
