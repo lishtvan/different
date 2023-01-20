129dba3a5c1f9be8256541f74912eb7561429ba8
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2023-01-20 19:17:53 +0200

    Small ui improvement

--- web/app/components/index/listings/Listings.tsx
+++ web/app/components/index/listings/Listings.tsx
@@ -26,7 +26,7 @@ const Listings = () => {
   }, [isLastPage, showMore]);
 
   return (
-    <div className="h-full w-full grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-x-[1.125rem] gap-y-4">
+    <div className="h-full w-full grid mt-3 md:mt-0 xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-x-[1.125rem] gap-y-4">
       {hits.map((listing) => (
         <Listing listing={listing} key={listing.objectID} />
       ))}
