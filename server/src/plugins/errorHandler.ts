70eba00a481adf25c4808bfa6f55c0af8c2e57df
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-02-14 21:04:49 +0200

    Add global error handler

---
+++ server/src/plugins/errorHandler.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin';
+
+export default fp(async (fastify) => {
+  fastify.setErrorHandler((error, request, reply) => {
+    if (!error.statusCode || error.statusCode >= 500) {
+      const statusCode = error.statusCode || 500;
+      fastify.log.error(error);
+      return reply.status(statusCode).send({
+        statusCode,
+        error: 'Internal Server Error',
+        message: 'Something went wrong',
+      });
+    } else throw error;
+  });
+});
