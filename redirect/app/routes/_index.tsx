937c04bab37c5f0889235c9e1d43ba7aeafd7b72
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-02-27 20:37:20 +0200

    Main page v2

--- redirect/app/routes/_index.tsx
+++ redirect/app/routes/_index.tsx
@@ -14,33 +14,39 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-screen container flex items-center justify-center">
-      <div className=" flex items-center gap-x-40">
+    <div className="h-screen container px-2 sm:px-2 flex items-center justify-center">
+      <div className="flex items-center justify-center gap-x-20 sm:gap-x-8 md:gap-x-8 lg:gap-x-32 xl:gap-x-40">
         <div className="flex flex-col">
-          <h1 className="font-bold leading-tight tracking-tighter md:text-2xl lg:leading-[1.1]">
+          <h1 className="text-center text-base font-bold leading-tight tracking-tighter md:text-2xl lg:leading-[1.1] sm:text-start">
             Different - маркетплейс для одягу, взуття <br /> та аксесуарів
           </h1>
-          <p className="max-w-[750px] mt-3 text-lg text-muted-foreground sm:text-xl">
-            Швидкі продажі та зручний пошук того, що вам потрібно.
+          <p className="text-center mt-3 text-sm text-muted-foreground lg:text-xl md:text-base sm:text-sm sm:text-start">
+            Швидкий, зручний, сучасний додаток для IOS та Android.
             <br />
-            Захищенність покупців та продавців.
-            <br />
-            Речі на будь-який смак - Vintage, New collections, <br /> Custom,
-            Streatwear, Outdoor.
+            Захищенність покупців та продавців. <br />
+            Vintage, New collections, Custom, Streatwear, Outdoor
+            <br /> - все для твого стилю.
           </p>
-          <div className="flex items-center mt-10">
+          <div className="flex items-center mt-10 gap-x-2 flex-col gap-y-5 sm:flex-row justify-center sm:justify-start">
             <Button size="lg" className="w-fit">
               Завантажити додаток
             </Button>
-            <Icons.apple className="h-8 w-8 ml-4 min-h-8 min-w-8" />
-            <img
-              src={android}
-              className="w-11 h-11 ml-1 mt-0.5"
-              alt="android"
-            />
+            <div className="flex items-center">
+              <Icons.apple className="h-8 w-8 min-h-8 min-w-8" />
+              <img
+                src={android}
+                className="w-11 h-11 ml-1 mt-0.5"
+                alt="android"
+              />
+            </div>
           </div>
         </div>
-        <img width={330} src={ios} alt="phone" />
+        <img
+          className="hidden sm:block sm:w-[220px] md:w-1/3 lg:w-1/4 xl:w-1/4"
+          width={330}
+          src={ios}
+          alt="phone"
+        />
       </div>
     </div>
   );
