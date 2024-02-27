4aa9203ac31458639c6726c8f4ab5343502b9a3d
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-02-27 22:41:18 +0200

    Main page v3

--- redirect/app/routes/_index.tsx
+++ redirect/app/routes/_index.tsx
@@ -16,16 +16,14 @@ export default function Index() {
   return (
     <div className="h-screen container px-2 sm:px-2 flex items-center justify-center">
       <div className="flex items-center justify-center gap-x-20 sm:gap-x-8 md:gap-x-8 lg:gap-x-32 xl:gap-x-40">
-        <div className="flex flex-col">
-          <h1 className="text-center text-base font-bold leading-tight tracking-tighter md:text-2xl lg:leading-[1.1] sm:text-start">
-            Different - маркетплейс для одягу, взуття <br /> та аксесуарів
+        <div className="flex flex-col max-w-[450px]">
+          <h1 className="mx-auto max-w-[300px] text-center text-base font-bold leading-tight tracking-tighter md:text-2xl lg:leading-[1.1] sm:text-start sm:max-w-full sm:mx-0">
+            Different - маркетплейс для одягу, взуття та аксесуарів
           </h1>
           <p className="text-center mt-3 text-sm text-muted-foreground lg:text-xl md:text-base sm:text-sm sm:text-start">
-            Швидкий, зручний, сучасний додаток для IOS та Android.
-            <br />
-            Захищенність покупців та продавців. <br />
-            Vintage, New collections, Custom, Streatwear, Outdoor
-            <br /> - все для твого стилю.
+            Швидкий, зручний, сучасний додаток для IOS та Android. Захищеність
+            покупців та продавців. Гнучкі фільтри пошуку, чат, трекінг
+            замовлення та багато іншого.
           </p>
           <div className="flex items-center mt-10 gap-x-2 flex-col gap-y-5 sm:flex-row justify-center sm:justify-start">
             <Button size="lg" className="w-fit">
