c317162598a6f53645295137760391bf1732684f
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-03-13 19:13:16 +0200

    Uninstall swagger ui

--- server/src/plugins/swagger.ts
+++ server/src/plugins/swagger.ts
@@ -1,6 +1,5 @@
 import fp from 'fastify-plugin';
 import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export default fp(async (fastify) => {
   if (process.env.NODE_ENV === 'production') return;
@@ -12,14 +11,5 @@ export default fp(async (fastify) => {
         version: '1.0.0',
       },
     },
-  });
-
-  await fastify.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
-    uiConfig: {
-      docExpansion: 'none',
-      deepLinking: false,
-    },
-    staticCSP: true,
   });
 });
