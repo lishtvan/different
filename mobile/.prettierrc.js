efe03faa7e6ba4976b3dbd2f6ae640b2f88de841
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-05-10 18:44:50 +0300

    Delete legacy mobile version

--- mobile/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-};
--- mobile/src/utils/secureStorage.ts
+++
@@ -1,6 +0,0 @@
-import EncryptedStorage from 'react-native-encrypted-storage';
-
-export const saveSession = (token: string, userId: number) =>
-  EncryptedStorage.setItem('session', JSON.stringify({ token, userId }));
-export const getSession = () => EncryptedStorage.getItem('session');
-export const destroySession = () => EncryptedStorage.removeItem('session');
