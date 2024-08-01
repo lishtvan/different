40ef2296aa0d85e0adfa725a3ebf86f68b5c2af9
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-08-01 20:30:07 +0300

    Format tracking number

--- server/src/routes/jobs/tracking.ts
+++ server/src/routes/jobs/tracking.ts
@@ -4,6 +4,10 @@ const schema = {
   tags: ['Jobs'],
 };
 
+const formatTrackingNumber = (trackingNumber: string) => {
+  trackingNumber.replace(/(\d{2})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4');
+};
+
 const getOrder: FastifyPluginAsync = async (fastify) => {
   fastify.post('/tracking', { schema }, async (req, reply) => {
     const orders = await fastify.prisma.order.findMany({
@@ -19,7 +23,7 @@ const getOrder: FastifyPluginAsync = async (fastify) => {
       recipientId: orders[0].sellerId, // or buyerId
       title: orders[0].Listing.title,
       // eslint-disable-next-line max-len
-      body: `Замовлення оплачено, відправте по номеру накладної ${orders[0].trackingNumber}`,
+      body: `Замовлення оплачено, відправте по номеру накладної ${formatTrackingNumber(orders[0].trackingNumber)}`,
       data: { type: 'order', orderId: orders[0].id, url: '_' },
     });
     return reply.send();
