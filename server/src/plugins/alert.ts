773ca7512cc2d9b687101ff558c68c3f8697c4b2
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-10-16 21:56:11 +0300

    Remove timestamp from alert

--- server/src/plugins/alert.ts
+++ server/src/plugins/alert.ts
@@ -4,15 +4,10 @@ export default fp(async (fastify) => {
   const url = `https://api.telegram.org/bot${process.env.NOTIFICATION_TG_BOT_TOKEN}/sendMessage`;
 
   const sendAlert = (text: string) => {
-    const formattedMsg = `${text} \nTimestamp: ${new Date().toLocaleString('uk-UA')}`;
     fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        chat_id: '820787098',
-        parse_mode: 'HTML',
-        text: formattedMsg,
-      }),
+      body: JSON.stringify({ chat_id: '820787098', parse_mode: 'HTML', text }),
     });
   };
 
